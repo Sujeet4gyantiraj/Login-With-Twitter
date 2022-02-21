efe30fc31cb50be5ba45e291fde0863cac7dad43
Sujeet4gyantiraj <98584026+Sujeet4gyantiraj@users.noreply.github.com>
2022-02-21 13:50:35 +0530

    Update settings.py

--- SocialAuth/settings.py
+++ SocialAuth/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bl%6h-jc^(^^jy8vjkmly45c^t-xol9$s)0s0a$g!!-z=bq*ml'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
