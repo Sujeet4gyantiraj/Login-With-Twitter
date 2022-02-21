76953d8aec49e2bdc8dad5c49dc9f0fd9fcecb6a
Sujeet4gyantiraj <98584026+Sujeet4gyantiraj@users.noreply.github.com>
2022-02-21 15:47:00 +0530

    Update settings.py

--- SocialAuth/settings.py
+++ SocialAuth/settings.py
@@ -51,7 +51,7 @@ SESSION_COOKIE_SECURE = True
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
